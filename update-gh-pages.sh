493c8c8af8b2654b335014ab2675647b720c2bc1
Artem Sapegin <artem@sapegin.ru>
2014-05-15 17:17:55 +0400

    Favicon.

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -6,7 +6,8 @@ grunt build
 cp docs/*.html ../tamia_gh/
 cp docs/styles.css ../tamia_gh/
 cp docs/scripts.js ../tamia_gh/
+cp docs/favicon.ico ../tamia_gh/
 pushd ../tamia_gh/
-git commit -m "Update docs." *.html styles.css scripts.js
+git commit -m "Update docs." *.html styles.css scripts.js favicon.ico
 git push
 popd
