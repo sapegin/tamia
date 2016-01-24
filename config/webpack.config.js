5f889a3ddb3424834a5b36b9d16ab3b1690fbdf0
Artem Sapegin <artem@sapegin.ru>
2016-01-24 08:40:54 +0100

    Add DEBUG const to Webpack.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
 	devtool: 'cheap-module-source-map',
@@ -21,6 +22,9 @@ module.exports = {
 	},
 
 	plugins: [
+		new webpack.DefinePlugin({
+			DEBUG: JSON.stringify('true'),
+		}),
 	],
 
 	module: {
