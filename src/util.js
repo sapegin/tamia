aa28f0b2974c39cbd15e246ed8a6c9b2b026896e
Artem Sapegin <artem@sapegin.ru>
2016-01-24 08:43:00 +0100

    TamiaError: remove unnecessary console.warn.

--- src/util.js
+++ src/util.js
@@ -1,12 +1,9 @@
-import { warn } from './debug/logger';
-
-// Custom exception
+/**
+ * Custom exception
+ */
 export class TamiaError extends Error {
 	constructor(message) {
 		super(message);
-		if (DEBUG) {
-			warn(...arguments);
-		}
 		this.name = this.constructor.name;
 		this.message = message;
 		Error.captureStackTrace(this, this.constructor.name);
