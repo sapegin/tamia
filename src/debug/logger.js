d5b99d572f4499872c6311e9f641231069fc4c50
Artem Sapegin <artem@sapegin.ru>
2016-01-24 08:41:28 +0100

    Add new logger module.

---
+++ src/debug/logger.js
@@ -0,0 +1,34 @@
+let log;
+let warn;
+let addBadge;
+
+if (DEBUG) {
+	addBadge = (args, name, background) => {
+		// Color console badge
+		// Based on https://github.com/jbail/lumberjack
+		if (navigator.userAgent.indexOf('Chrome') !== -1 || navigator.userAgent.indexOf('Firefox') !== -1) {
+			let format = '%c %s %c ' + args.shift();
+			args.unshift(format, `background:${background}; color:#fff`, name, 'background:inherit; color:inherit');
+		}
+		else {
+			args[0] = `${name}: ${args[0]}`;
+		}
+		return args;
+	};
+
+	const logger = (func) => (...args) => {
+		console[func](...addBadge(args, 'Tâmia', '#aa759f'));  // eslint-disable-line no-console
+	};
+
+	log = logger('log');
+	warn = logger('warn');
+}
+else {
+	log = warn = addBadge = function() {};
+}
+
+export {
+	log,
+	warn,
+	addBadge,
+};
