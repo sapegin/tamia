bda70430e04e06cf76cd520cbed33eca364c0fde
Artem Sapegin <artem@sapegin.ru>
2016-02-21 21:11:59 +0100

    Expose public API.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './styles/index.styl';
+
+// Expose public API
+export {
+	registerAnimations,
+	runAnimation,
+	afterTransitions,
+} from './animation';
+export {
+	appear,
+	disappear,
+	toggle,
+} from './appear';
+export {
+	Component,
+} from './Component';
+export {
+	default,
+} from './data';
+export {
+	onEvent,
+	offEvent,
+	triggerEvent,
+	triggerNativeEvent,
+	registerGlobalEvents,
+} from './events';
+export {
+	oporElement,
+	oporClass,
+} from './opor';
+export {
+	default,
+} from './registerComponent';
+export {
+	toggleState,
+	addState,
+	removeState,
+	hasState,
+} from './states';
