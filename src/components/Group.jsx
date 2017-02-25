2dc7a05301146b8566625c569624ba2cc71efd98
Artem Sapegin <artem@sapegin.ru>
2017-02-25 21:07:55 +0100

    Fix(Group): glue → separator, do not pass children to div as a prop

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -1,12 +1,18 @@
-export default function Group({ glue = ' ', inline = false, ...rest }, children) {
+export default function Group({
+	separator = ' ',
+	inline = false,
+	component = inline ? 'span' : 'div',
+	children,
+	...rest
+}) {
 	const items = [];
 	children = children.filter(child => !!child);
 	children.forEach((item, index) => {
 		items.push(item);
 		if (index < children.length - 1) {
-			items.push(glue);
+			items.push(separator);
 		}
 	});
-	const Tag = inline ? 'span' : 'div';
+	const Tag = component;
 	return <Tag {...rest}>{items}</Tag>;
 }
