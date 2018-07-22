1fc27252b3f8c49194bbbf0efa3c46439744fb1f
Artem Sapegin <artem@sapegin.ru>
2018-07-22 14:05:26 +0200

    Heading: Fix blacklist prop

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
+import Base from './Base';
 
 const SIZES = ['xxl', 'xl', 'l', 'm', 's', 'xs'];
 
 // eslint-disable-next-line react/prop-types
-const Base = ({ is, level, ...props }) => {
-	const Component = is || `h${level}`;
-	return <Component {...props} />;
-};
+const HeadingBase = ({ is, level, ...props }) => (
+	<Base is={is || `h${level}`} {...props} />
+);
 
-const Heading = styled(Base)`
+const Heading = styled(HeadingBase)`
 	margin-top: ${props => props.theme.space[props.mt] || 0};
 	margin-bottom: ${props => props.theme.space[props.mb] || 0};
 	font-size: ${props => props.theme.fontSizes[SIZES[props.level - 1]]};
