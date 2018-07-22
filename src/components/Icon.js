9cdd813963e2dd020b9cea28b6ccd672f415a238
Artem Sapegin <artem@sapegin.ru>
2018-07-22 20:39:19 +0200

    New component: Icon

---
+++ src/components/Icon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+
+const Svg = styled('svg')`
+	display: inline-block;
+	vertical-align: middle;
+`;
+
+const Icon = ({ icon, width, height, ...props }) => {
+	return (
+		<Svg
+			{...props}
+			viewBox={`0 0 ${icon.width} ${icon.height}`}
+			width={width}
+			height={height}
+			fill="currentColor"
+			preserveAspectRatio="xMidYMid meet"
+		>
+			<path d={icon.path} />
+		</Svg>
+	);
+};
+
+Icon.propTypes = {
+	icon: PropTypes.shape({
+		path: PropTypes.string.isRequired,
+		width: PropTypes.number.isRequired,
+		height: PropTypes.number.isRequired,
+	}).isRequired,
+	width: PropTypes.number.isRequired,
+	height: PropTypes.number.isRequired,
+};
+
+export default Icon;
