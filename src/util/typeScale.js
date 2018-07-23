295cfad50c86e1f6c687a525f170db2bf764e5a7
Artem Sapegin <artem@sapegin.ru>
2018-07-23 06:24:11 +0200

    Make typography scale value a bit shorter

--- src/util/typeScale.js
+++ src/util/typeScale.js
@@ -3,7 +3,9 @@ import { modularScale } from 'polished';
 // Generate typography scale
 const getTypeScale = (sizes, ratio) => {
 	return Object.keys(sizes).reduce((scale, size) => {
-		scale[size] = modularScale(sizes[size], 1, ratio).replace('em', 'rem');
+		// Convert to rems and remove unnecessary precision
+		const value = parseFloat(modularScale(sizes[size], 1, ratio));
+		scale[size] = `${Math.round(value * 1000) / 1000}rem`;
 		return scale;
 	}, {});
 };
