6a78a76a94fbc52c5c9d55d4c0246b19f664c6a6
Artem Sapegin <artem@sapegin.ru>
2020-02-25 09:50:09 +0100

    feat: Make Icon component more flexible
    - Remove icon.path prop, pass <path d="..."/> as children
    - Rename icon.width and icon.height props to viewBox.width and viewBox.height

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,8 +5,7 @@ import { space, layout, SpaceProps, LayoutProps } from 'styled-system';
 const Svg = styled.svg<SpaceProps & LayoutProps>(space, layout);
 
 type Props = SpaceProps & {
-	icon: {
-		path: string;
+	viewBox: {
 		width: number;
 		height: number;
 	};
@@ -20,15 +19,14 @@ type Props = SpaceProps & {
 /**
  * Generic SVG icon component.
  */
-const Icon = ({ icon, children, ...props }: Props) => {
+const Icon = ({ viewBox, children, ...props }: Props) => {
 	return (
 		<Svg
 			{...props}
-			viewBox={`0 0 ${icon.width} ${icon.height}`}
+			viewBox={`0 0 ${viewBox.width} ${viewBox.height}`}
 			fill="currentColor"
 			preserveAspectRatio="xMidYMid meet"
 		>
-			<path d={icon.path} />
 			{children}
 		</Svg>
 	);
