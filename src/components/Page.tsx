3e860ffa43990b0b67ac31641bb31349593d51f1
Artem Sapegin <artem@sapegin.ru>
2021-08-09 11:28:23 +0200

    fix(Page): Remove extra padding that's conflicting with Container component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,11 +5,11 @@ const PageStyled = styled.div`
 	display: flex;
 	flex-direction: column;
 	min-height: 100vh;
-	padding: ${(p) => p.theme.page.contentPaddingY} 0;
 `;
 
 const FooterStyled = styled.footer`
 	margin-top: auto;
+	padding-top: ${(p) => p.theme.page.contentPaddingY};
 `;
 
 /**
