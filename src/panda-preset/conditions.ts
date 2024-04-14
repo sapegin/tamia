9798c94b211cf77e7a552c10666de3f0432b1c38
Artem Sapegin <artem@sapegin.ru>
2024-04-14 12:45:55 +0200

    feat: New condition _pointerFine
    To detect when the main input mechanism has high precision (for example,
    mouse or trackpad).

--- src/panda-preset/conditions.ts
+++ src/panda-preset/conditions.ts
@@ -3,5 +3,8 @@ import base from '@pandacss/preset-base';
 // Conditions
 export const conditions = {
 	...base.conditions,
+	/** Override default _hover condition to skip hover disabled elements */
 	hover: '&:hover:not(:disabled)',
+	/** Main input mechanism has high precision (for example, mouse or trackpad) */
+	pointerFine: '@media (pointer: fine)',
 };
